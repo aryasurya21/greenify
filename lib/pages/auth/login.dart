6ca79abf7f6f51c1f2444e1a14a5cbdf9125eebb
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2020-06-28 02:22:56 +0700

    Added login and register page funcionalities

---
+++ lib/pages/auth/login.dart
@@ -0,0 +1,251 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
+import 'package:greenify/pages/auth/register.dart';
+import 'package:greenify/pages/home/home.dart';
+import 'package:greenify/util/session_util.dart';
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String _email, _password;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.black,
+      ),
+      child: Scaffold(
+        resizeToAvoidBottomPadding: false,
+        backgroundColor: Colors.transparent,
+        body: Form(
+          key: _formKey,
+          child: Align(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Container(
+                  height: 50,
+                  margin: const EdgeInsets.only(
+                    bottom: 25.0,
+                  ),
+                  child: new Image.asset(
+                    'assets/graphics/greenify_logo.png'
+                  ),
+                ),
+                Text(
+                  'Login',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 28
+                  ),
+                ),
+                new Container(
+                  margin: const EdgeInsets.all(
+                    10.0,
+                  ),
+                  child: new SizedBox(
+                    width: 275.0,
+                    child: TextFormField(
+                      cursorColor: Colors.white,
+                      validator: (input){
+                        if(input.isEmpty){
+                          return 'Please type an email';
+                        }
+                      },
+                      onSaved: (input) => _email = input,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        labelStyle: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                new Container(
+                  margin: const EdgeInsets.all(
+                    10.0,
+                  ),
+                  child: new SizedBox(
+                    width: 275.0,
+                    child: TextFormField(
+                      cursorColor: Colors.white,
+                      validator: (input){
+                        if(input.isEmpty){
+                          return 'Please provide a password';
+                        }
+                        else if(input.length < 6){
+                          return 'Your password needs to be atleast 6 characters';
+                        }
+                      },
+                      onSaved: (input) => _password = input,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        labelStyle: TextStyle(
+                          color: Colors.white,
+                        ),
+                        enabledBorder: new UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white, 
+                            width: 1.0, 
+                            style: BorderStyle.none 
+                          ),
+                        ),
+                      ),
+                      obscureText: true,
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                new Container(
+                  margin: const EdgeInsets.all(
+                    10.0,
+                  ),
+                  child: new SizedBox(
+                    width: 255.0,
+                    child: RaisedButton(
+                      onPressed: signIn,
+                      padding: EdgeInsets.all(10.0),
+                      color: Colors.white,
+                      child: Text(
+                        'Play',
+                        style: new TextStyle(
+                          fontSize: 24.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                new InkWell(
+                    child: Text(
+                      'Don\'t have an account? Register here',
+                      style: new TextStyle(
+                        fontSize: 16.0, 
+                        color: Colors.white,
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (context) => RegisterPage())
+                      );
+                    },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> signIn() async{
+    final formState = _formKey.currentState;
+
+    if(formState.validate()){
+      formState.save();
+      try{
+        AuthResult authResult  = await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: _email,
+          password: _password
+        );
+        FirebaseUser user = authResult.user;
+        if(user.isEmailVerified){
+          saveUserLogin(user);
+          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage()));
+        }
+        else{
+          return showDialog<void>(
+            context: context,
+            barrierDismissible: false, // user must tap button!
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text(
+                  'Error',
+                  style: new TextStyle(
+                    color: Colors.red[600],
+                  ),
+                ),
+                content: SingleChildScrollView(
+                  child: ListBody(
+                    children: <Widget>[
+                      Text('Email is not verified.\nPlease verify your email before login!'),
+                    ],
+                  ),
+                ),
+                actions: <Widget>[
+                  FlatButton(
+                    child: Text(
+                      'Close',
+                      style: new TextStyle(
+                        color: Colors.red[600],
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        }
+      }
+      catch(signInError){
+        if(signInError is PlatformException) {
+          if(signInError.code == 'ERROR_WRONG_PASSWORD') {
+            return showDialog<void>(
+              context: context,
+              barrierDismissible: false, // user must tap button!
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text(
+                    'Error',
+                    style: new TextStyle(
+                      color: Colors.red[600],
+                    ),
+                  ),
+                  content: SingleChildScrollView(
+                    child: ListBody(
+                      children: <Widget>[
+                        Text('Wrong password!'),
+                      ],
+                    ),
+                  ),
+                  actions: <Widget>[
+                    FlatButton(
+                      child: Text(
+                        'Close',
+                        style: new TextStyle(
+                          color: Colors.red[600],
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ],
+                );
+              },
+            );
+          }
+        }
+      }
+    }
+  }
+}
--- lib/pages/splash/splash.dart
+++ lib/pages/splash/splash.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:greenify/pages/auth/login.dart';
 
 class SplashPage extends StatefulWidget {
   @override
@@ -11,39 +12,46 @@ class _SplashState extends State<SplashPage> {
   Widget build(BuildContext context){
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: new Image.asset(
-                'assets/graphics/spinning_globe.gif',
-                width: 150.0,
-                height: 150.0,
-              )
-            ),
-            Container(
-              width: double.infinity,
-              margin: const EdgeInsets.only(top: 20.0),
-              child: new Image.asset(
-                'assets/graphics/greenify_logo.png',
-                height: 50.0,
-              )
-            ),
-            Container(
-              width: double.infinity,
-              margin: const EdgeInsets.only(top: 20.0),
-              child: Text(
-                'Touch To Start',
-                textAlign: TextAlign.center,
-                style: GoogleFonts.pressStart2p(
-                  color: Colors.white,
+      body: GestureDetector(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                child: new Image.asset(
+                  'assets/graphics/spinning_globe.gif',
+                  width: 150.0,
+                  height: 150.0,
+                )
+              ),
+              Container(
+                width: double.infinity,
+                margin: const EdgeInsets.only(top: 20.0),
+                child: new Image.asset(
+                  'assets/graphics/greenify_logo.png',
+                  height: 50.0,
+                )
+              ),
+              Container(
+                width: double.infinity,
+                margin: const EdgeInsets.only(top: 20.0),
+                child: Text(
+                  'Touch To Start',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.pressStart2p(
+                    color: Colors.white,
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
+        onTap: (){
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => LoginPage())
+          );
+        },
       ),
     );
   }
