beefb5f2c0320c3249620432a160a4633807c3d4
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2020-06-28 19:27:50 +0700

    Added text animation and session check

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:greenify/pages/auth/login.dart';
+import 'package:greenify/pages/home.dart';
+import 'package:greenify/util/session_util.dart';
 
 class SplashPage extends StatefulWidget {
   @override
@@ -8,6 +12,17 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashState extends State<SplashPage> {
+  bool _visible = true;
+
+  _SplashState() {
+    Timer.periodic(
+      Duration(milliseconds: 700), 
+      (Timer t) => setState((){
+        _visible = !_visible;
+      })
+    );
+  }
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -36,11 +51,15 @@ class _SplashState extends State<SplashPage> {
               Container(
                 width: double.infinity,
                 margin: const EdgeInsets.only(top: 20.0),
-                child: Text(
-                  'Touch To Start',
-                  textAlign: TextAlign.center,
-                  style: GoogleFonts.pressStart2p(
-                    color: Colors.white,
+                child: AnimatedOpacity(
+                  opacity: _visible ? 1.0 : 0.0,
+                  duration: Duration(milliseconds: 500),
+                  child: Text(
+                    'Touch To Start',
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.pressStart2p(
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
@@ -48,9 +67,12 @@ class _SplashState extends State<SplashPage> {
           ),
         ),
         onTap: (){
-          Navigator.of(context).pushReplacement(
-            MaterialPageRoute(builder: (context) => LoginPage())
-          );
+          getUserLogin().then((authUid) => {
+            if(authUid != null && authUid != "")
+              Navigator.of(context).pushReplacement( MaterialPageRoute(builder: (context) => HomePage()) )
+            else
+              Navigator.of(context).pushReplacement( MaterialPageRoute(builder: (context) => LoginPage()) )
+          });
         },
       ),
     );
